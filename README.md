1da8f1573c398f03fe71215140608be66cfe8f7c
James Trayford <j.w.trayford@port.ac.uk>
2024-12-12 10:26:27 +0000

    exploratory script for parsing astro-ph

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # astro-ph-bots
 A collection of bots that post new papers added to astro-ph to Bluesky.
+
+## `newsub`-bot
+A Bluesky bot that posts the abstract link and metadata of new submissions each day.
---
+++ newsubs/hello_arxiv.py
@@ -0,0 +1,102 @@
+import arxiv
+from datetime import datetime, timedelta, timezone
+from pylatexenc.latex2text import LatexNodes2Text
+
+CHARLIM = 300
+
+client = arxiv.Client()
+
+subcats = ['CO','EP','GA','HE','IM','SR']
+subcat_emoji = ['🔮','🪐','🌀','🎆','🛠️','✨']
+emojidict = dict(zip(subcats, subcat_emoji))
+
+def formatTex(s):
+    """
+    pre-pre-processs and then convert any TeX -> Unicode
+    """
+    deamp = s.replace('&', '＆')
+    return LatexNodes2Text().latex_to_text(deamp)
+
+def formatAuthors(authors, shortnames=False):
+    """
+    format the author list
+    """
+    namestring = ''
+    trunc = None
+    nauth = len(authors)
+    if nauth > 3:
+        trunc = 1
+        delims = []
+    elif nauth != 1:
+        delims = [","]*(len(authors[:trunc]) - 2) + [" &"]
+    else:
+        delims = []
+    delims += [""]
+    i = 0
+    for auth in authors[:trunc]:
+        if shortnames:
+            # can shorten names for space, but
+            # seems hard to do this reliably everywhere
+            fullname = auth.name.split(' ')
+            for name in fullname[:-1]:
+                if name[0].islower():
+                    namestring += f"{name}"
+                else:
+                    namestring += f"{name[0]}. "
+                if '-' in name:
+                    namestring += f"-{name.split('-')[-1][0]}. "
+            namestring += f"{fullname[-1]}{delims[i]} "
+        else:
+            namestring += f"{auth.name}{delims[i]} "
+        i+=1
+    if trunc:
+        namestring = namestring[:-1] + " et al."
+    return namestring
+
+now = datetime.now(timezone(timedelta(hours=-5)))
+query = "cat:astro-ph.*"
+
+search = arxiv.Search(
+  query = query,
+  max_results = 200,
+  sort_by = arxiv.SortCriterion.SubmittedDate
+)
+
+results = client.results(search)
+
+posts = []
+feeds = []
+
+for paper in results:
+    post = []
+    if (now-paper.published).days >= 1:
+        continue
+    emojis = ''
+    feedlist = []
+    for c in paper.categories:
+        if "astro-ph." in c:
+            feed = c.split('.')[-1]
+            # add emoji!
+            emojis += emojidict[feed]
+            feedlist.append(feed)
+    post.append(f"{formatTex(paper.title)}")
+    namestring = formatAuthors(paper.authors, shortnames=False)
+    post.append(f"{namestring}")
+    post.append(f"{paper.pdf_url} {emojis}")
+    full_len = len('\n'.join(post))
+    if full_len > CHARLIM:
+        char_surplus =  full_len - CHARLIM + 3
+        title_words = post[0].split(" ")
+        while char_surplus >= 0 :
+            char_surplus -= len(title_words[-1])
+            del title_words[-1]
+        post[0] = ' '.join(title_words) + "..."
+    post_string = '\n'.join(post)
+    posts.append(post_string)
+    feeds.append(feedlist)
+    print(post_string)
+    print(f"[posted {paper.published} - was {full_len} of {CHARLIM} characters, reduced by {full_len-len(post_string)}]\n")
+
+# print("="*60)
+# print(posts)
+# print(feeds)
